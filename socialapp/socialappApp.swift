fe3a838035ddd925a7bb1e972b86b5b6ade54751
Vincent Fleming <flemingvincent@ufl.edu>
2023-10-11 13:32:51 -0400

    Initial Commit

---
+++ socialapp/socialappApp.swift
@@ -0,0 +1,17 @@
+//
+//  socialappApp.swift
+//  socialapp
+//
+//  Created by Vincent Fleming on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct socialappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
